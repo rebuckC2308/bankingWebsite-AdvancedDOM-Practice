039d7175764b18169185514942a70d831a2331fd
Chris Rebuck <rebuckc2308@gmail.com>
2021-11-28 16:26:19 -0600

    Added functionality for modal window, created cookie message element, implemented smooth scrolling to section 1

--- script.js
+++ script.js
@@ -1,4 +1,58 @@
-"use strict";
+'use strict';
 ////////////////////////////////////////
 
 //Elements:
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const header = document.querySelector('.header');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+//Modal Window functionality:
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+//Creating and Inserting a cookie message element:
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.append(message);
+
+//Removing Cookie Message:
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+    message.remove();
+  });
+
+//Implementing Smooth Scrolling:
+btnScrollTo.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
